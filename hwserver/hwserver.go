252918b4a0cf4559cc00448b3675176fb9cc3233
Gawain <gawainli@outlook.com>
2015-11-10 23:41:30 +0800

    move hwserver.go to hwserver folder

---
+++ hwserver/hwserver.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+    zmq "github.com/pebbe/zmq4"
+    "fmt"
+    "time"
+)
+
+func main(){
+    responder, _ := zmq.NewSocket(zmq.REP)
+    defer responder.Close()
+    responder.Bind("tcp://*:5555")
+
+    for{
+        msg, _ := responder.Recv(0)
+        fmt.Println("Received ", msg)
+
+        time.Sleep(time.Second)
+
+        reply := "World"
+        responder.Send(reply, 0)
+    }
+}
